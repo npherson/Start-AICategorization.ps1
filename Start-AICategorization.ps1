6baf2c28e20b510b1c92d3ad30b3c334427b26ee
Nash Pherson <nashp@nowmicro.com>
2016-10-12 16:08:02 -0700

    Fixed whatif

--- Start-AICategorization.ps1
+++ Start-AICategorization.ps1
@@ -58,9 +58,9 @@ Param
     [Switch]$SyncCatalog = $False,
     [ValidateNotNullOrEmpty()]
     [ValidateRange(1,9999)]
-    [Int]$Limit = 9999,
+    [Int]$Limit = 1,
     [ValidateNotNullOrEmpty()]
-    [String]$IgnoreString = "",
+    [String]$IgnoreString = '',
     [Switch]$HideSummary = $False
     
 )
@@ -70,35 +70,35 @@ Begin
     $start = Get-Date
 
     # Find the site code, error out if we are not on the primary...
-    Write-Progress -Activity "Requesting Categorization" -Status "Getting the site code" -PercentComplete 0
+    Write-Progress -Activity 'Requesting Categorization' -Status 'Getting the site code' -PercentComplete 0
     $SiteCode = ''
-    Get-WMIObject -Namespace "root\SMS" -Class "SMS_ProviderLocation" -ErrorAction SilentlyContinue | foreach-object { if ($_.ProviderForLocalSite -eq $true){$SiteCode=$_.sitecode} }
-    If ($SiteCode -eq ''){Throw "Could not determine site code. Ensure you are running this script elevated while on the Primary Site Server. It is not designed to run remotely or without elevation."}
+    Get-WMIObject -Namespace 'root\SMS' -Class SMS_ProviderLocation -ErrorAction SilentlyContinue | foreach-object { if ($_.ProviderForLocalSite -eq $true){$SiteCode=$_.sitecode} }
+    If ($SiteCode -eq ''){Throw 'Could not determine site code. Ensure you are running this script elevated while on the Primary Site Server. It is not designed to run remotely or without elevation.'}
 
     # Pull the AI summary...
-    Write-Progress -Activity "Requesting Categorization" -Status "Gathering summary of AI classification status" -PercentComplete 1
-    Write-Verbose "Getting summary of AI classification status before running..."
-    $summaryBefore = Invoke-WmiMethod -class SMS_AISoftwarelist -namespace Root\SMS\Site_$($siteCode) -name GetSummary
+    Write-Progress -Activity 'Requesting Categorization' -Status 'Gathering summary of AI classification status' -PercentComplete 1
+    Write-Verbose -Message 'Getting summary of AI classification status before running...'
+    $summaryBefore = Invoke-WmiMethod -class SMS_AISoftwarelist -namespace Root\SMS\Site_$($siteCode) -name GetSummary -WhatIf:$false
 
     # Pull the AI pending list...
-    Write-Progress -Activity "Requesting Categorization" -Status "Gathering list of applications from AI that are pending classification" -PercentComplete 2
-    Write-Verbose "Getting list of applications from AI that are pending classification..."
-    $appsList = Get-WmiObject -Namespace Root\SMS\Site_$($siteCode) -Class SMS_AISoftwarelist -Filter "State = 4"
-    Write-Verbose "Unsent categorization requests: $($apps.Count)"
+    Write-Progress -Activity 'Requesting Categorization' -Status 'Gathering list of applications from AI that are pending classification' -PercentComplete 2
+    Write-Verbose -Message 'Getting list of applications from AI that are pending classification...'
+    $appsList = Get-WmiObject -Namespace Root\SMS\Site_$($siteCode) -Class SMS_AISoftwarelist -Filter 'State = 4'
+    Write-Verbose -Message "Unsent categorization requests: $($apps.Count)"
 
 
     # Determine if we can send all the pending or if we need to limit it...
-    Write-Verbose "Determine how many records we can send for synchronization..."
+    Write-Verbose -Message 'Determine how many records we can send for synchronization...'
     If ($limit -ge 10000)
     {
-        Write-Verbose "The daily limit for sending records is 10,000. Setting the limit for this script to 9,999 or the total number of pending items, whichever is less."
+        Write-Verbose -Message 'The daily limit for sending records is 10,000. Setting the limit for this script to 9,999 or the total number of pending items, whichever is less.'
         $limit = 9999
     }
     $max = $limit
 
 
     # Send the list for categorization...
-    Write-Verbose "Attempting to categorize pending software..."
+    Write-Verbose -Message 'Attempting to categorize pending software...'
     $i = 0
     foreach ($app in $appsList)
     {
@@ -108,56 +108,49 @@ Begin
             $secondsElapsed = (Get-Date) - $start
             $secondsRemaining = ($secondsElapsed.TotalSeconds / $i) * ($max - $i)
 
-            Write-Progress -Activity "Requesting Categorization" -Status "Sending $i of $max - State $($app.State) - $($app.commonname)" -PercentComplete (($i / $max)*100) -SecondsRemaining $secondsRemaining
-            Write-Verbose "Sending $i of $max - State $($app.State) - $($app.commonname) - $($App.SoftwareKey)"
+            Write-Progress -Activity 'Requesting Categorization' -Status "Sending $i of $max - State $($app.State) - $($app.commonname)" -PercentComplete (($i / $max)*100) -SecondsRemaining $secondsRemaining
+            Write-Verbose -Message "Sending $i of $max - State $($app.State) - $($app.commonname) - $($App.SoftwareKey)"
 
-            If ($app.commonname -like "*$($ignoreString)*")
+            If ($app.commonname -like "*$($ignoreString)*" -And $ignoreString -ne '')
             {
-                Write-Warning "Not sending $($app.commonname) because it contains an ignored string: *$($ignoreString)*"
+                Write-Warning -Message "Not sending $($app.commonname) because it contains an ignored string: *$($ignoreString)*"
             } 
-            ElseIf ($pscmdlet.ShouldProcess('WHATIF Request categorization', $app.commonname))
+            ElseIf ($pscmdlet.ShouldProcess($app.commonname, 'WHATIF Request categorization'))
             {
                 $request = Invoke-WmiMethod -class SMS_AISoftwarelist -namespace Root\SMS\Site_$($siteCode) -name SetCategorizationRequest -ArgumentList $app.softwarekey
-                If ($request.ReturnValue -eq 0) {Write-Verbose "Status $($request.ReturnValue) for $($app.commonname)"} Else {Write-Warning "Return Status $($request.ReturnValue) for $($app.commonname)."}
+                If ($request.ReturnValue -eq 0) {Write-Verbose -Message "Status $($request.ReturnValue) for $($app.commonname)"} Else {Write-Warning -Message "Return Status $($request.ReturnValue) for $($app.commonname)."}
             }
         }
         Else
         {
             # Made it to the max or limit...
-            Write-Verbose "Synced the maximum number of entries (-Limit, All, or the daily max of 9999): $($Max)"
+            Write-Verbose -Message "Synced the maximum number of entries (-Limit, All, or the daily max of 9999): $($Max)"
             Break    
         }
     }
 
     # Pull the AI summary after requesting categorization...
-    Write-Progress -Activity "Requesting Categorization" -Status "Gathering summary of AI classification status" -PercentComplete 100
-    Write-Verbose "Getting summary of AI classification status before running..."
-    $summaryAfter = Invoke-WmiMethod -class SMS_AISoftwarelist -namespace Root\SMS\Site_$($siteCode) -name GetSummary
-    Write-Progress -Activity "Requesting Categorization" -Completed
-
-    Write-Verbose "Beginning Statistics:"
-    write-Verbose $summaryBefore
-
-    Write-Verbose "Ending Statistics:"
-    write-Verbose $summaryAfter
+    Write-Progress -Activity 'Requesting Categorization' -Status 'Gathering summary of AI classification status' -PercentComplete 100
+    Write-Verbose -Message 'Getting summary of AI classification status after running...'
+    $summaryAfter = Invoke-WmiMethod -class SMS_AISoftwarelist -namespace Root\SMS\Site_$($siteCode) -name GetSummary -WhatIf:$false
+    Write-Progress -Activity 'Requesting Categorization' -Completed
 
     $secondsElapsed = (Get-Date) - $start
     #$totalTime =  $secondsElapsed.ToString("hh\:mm\:ss")
-    $totalTime = $secondsElapsed.ToString("hh\ \h\o\u\r\s\ mm\ \m\i\n\ ss\ \s\e\c")
-    write-host "Summary Information"
+    $totalTime = $secondsElapsed.ToString('hh\ \h\o\u\r\s\ mm\ \m\i\n\ ss\ \s\e\c')
+    write-host 'Summary Information'
     write-host "Before: $($SummaryBefore.Uncategorized)"
     write-host "After:  $($SummaryAfter.Uncategorized)"
     write-host "Tried:  $($i)"
     write-host "Got:    $(($SummaryBefore.Uncategorized) - $($SummaryAfter.Uncategorized))"
     write-host "Time Elapsed:  $($totalTime)"
-    $appsList = ""
 
 
     # Tell the AI Sync Point to synchronize...
     If ($SyncCatalog)
     {
-        Write-Progress -Activity "Requesting Categorization" -Status "Telling AI Sync Point to synchronize at next polling interval." -PercentComplete 100
-        Write-Verbose "Flagging AI service to start synchronizing at next cycle. Default polling interval is 900 seconds. Monitor AIUpdateSvc.log and aikbmgr.log for status."
+        Write-Progress -Activity 'Requesting Categorization' -Status 'Telling AI Sync Point to synchronize at next polling interval.' -PercentComplete 100
+        Write-Verbose -Message 'Flagging AI service to start synchronizing at next cycle. Default polling interval is 900 seconds. Monitor AIUpdateSvc.log and aikbmgr.log for status.'
         If ($pscmdlet.ShouldProcess('WHATIF Start sync', $app.commonname)) {Invoke-WmiMethod -class SMS_AIProxy -namespace Root\SMS\Site_$($siteCode) -name RequestCatalogUpdate}
     }
 }
